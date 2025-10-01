4ea1e34c3c94e90d34118e1499c430086571b08a
JonasStokkeland <js.jonas97@gmail.com>
2025-10-01 20:35:33 +0200

    add function to sort char dictionaries in a list descending, add function to format the counts of characters and print report

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from stats import count_words
 from stats import char_counter
+from stats import create_list_of_dictionaries
+
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,11 +12,37 @@ def get_book_text(filepath):
 
 
 
-def main(): 
-    text = get_book_text("books/frankenstein.txt")
-    
-    num_words = count_words(text)
-    print(f"Found {num_words} total words")
-    print(char_counter(text))
 
+
+
+
+def format_char_counts(list):
+    lines = []
+    for item in list:
+        if str(item["char"]).isalpha():
+            lines.append( f"{item['char']}: {item['num']}")
+    return "\n".join(lines)
+
+       
+
+
+def main(): 
+    path= "books/frankenstein.txt"
+    text = get_book_text(path)
+    to_be_printed = create_list_of_dictionaries(char_counter(text))
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(text)} total words")
+    print("--------- Character Count -------")
+    print(format_char_counts(to_be_printed))
+    print("============= END ===============")
 main()
+
+
+
+#path= "books/frankenstein.txt"
+#dict = char_counter(get_book_text(path))
+#list_of_items = create_list_of_dictionaries(dict)
+#convert(list_of_items)
--- stats.py
+++ stats.py
@@ -8,6 +8,22 @@ def char_counter(text):
         counts[char] = counts.get(char, 0) + 1
 
     return counts
-  
-    
 
+test_dictionary = {"t" : 29493, "h": 19176, "e": 44538}
+#test_dictionary_list =[{"char": "t", "num", 29493}, {"char": "h", "num", 19176}, {"char": "t", "num", 445383}]
+
+def get_num_key(item):
+    return item["num"]
+
+def create_list_of_dictionaries(dictionary):
+
+    list_of_dictionaries = []
+    new_dictionary ={}
+
+    for key, value in dictionary.items():
+        new_dictionary = {"char" : key, "num" : value}
+        list_of_dictionaries.append(new_dictionary)
+    
+    list_of_dictionaries.sort(reverse = True, key = get_num_key)
+    return list_of_dictionaries
+    
